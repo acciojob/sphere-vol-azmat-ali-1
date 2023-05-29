fa3d7827f5f8680aeda1a7914945da3871081803
Azmat-Ali-1 <114492086+azmat-ali-1@users.noreply.github.com>
2023-05-29 17:47:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function volume_sphere() {
     //Write your code here
  const radius = document.getElementById("radius").value;
   const vol = (4 / 3) * Math.PI * Math.pow(radius, 3);
-  document.getElementById("volume").value = vol;
+  document.getElementById("volume").value = vol.toFixed(2);
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
